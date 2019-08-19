2fbb852a0ab35002f54832498544192384a48e88
Monilal <monilal.kumbhakar@iqgateway.com>
2019-08-19 13:29:35 +0530

    first commit

---
+++ src/components/counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <md-button class="md-raised md-primary" @click="increase"
+      >Increase</md-button
+    >
+    <md-button class="md-raised md-accent" @click="decrease"
+      >Decrease</md-button
+    >
+    <md-button class="md-raised md-primary" @click="asyncIncrease(5)"
+      >Async Increase by 5</md-button
+    >
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "app-counter",
+  methods: {
+    ...mapActions({
+      increase: "increase",
+      decrease: "decrease"
+    }),
+    asyncIncrease(incrementalValue) {
+      const objectValue = {
+        incrementalValue
+      };
+      this.$store.dispatch("asyncIncrement", objectValue);
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  margin: 10px;
+  width: 170px;
+  height: 60px;
+  font-size: 40px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -21,6 +21,17 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/state-counter",
+      name: "state-counter",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(
+          /* webpackChunkName: "state-counter" */ "./views/StateCounter.vue"
+        )
     }
   ]
 });
